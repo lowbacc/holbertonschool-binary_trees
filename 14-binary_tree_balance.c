e4b787d37c7e5d4e7e6018ab0df6a19e8f981515
lowbacc <skalski.matthis@gmail.com>
2024-08-09 21:18:17 +0200

    Task 14, my last return was with tree and not height, I fixed it

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -25,8 +25,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int left_height, right_height;
+
 	if (tree == NULL)
 		return (0);
 
-	return (int)(binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	left_height = (tree->left) ? (int)binary_tree_height(tree->left) : 0;
+	right_height = (tree->right) ? (int)binary_tree_height(tree->right) : 0;
+
+	return (left_height - right_height);
 }
